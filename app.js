bb35fe02ccb2f9f80eac1d0f64dce876e318260c
Edem <edem.quashigah@student.hamk.fi>
2026-03-19 22:38:06 +0200

    Health check proper error

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/health', (req, res) => {
   // eslint-disable-next-line no-constant-condition
   if (true) throw 'error...  ';
   res.send('ok');
-};);
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
