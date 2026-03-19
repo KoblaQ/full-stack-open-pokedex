41d5c1ac245a04f537ecd64c59bc66f2e1d8707c
Edem <edem.quashigah@student.hamk.fi>
2026-03-19 22:45:04 +0200

    fixed Health Check

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('dist'));
 
 app.get('/version', (req, res) => {
-  res.send('2'); // change this string to ensure a new version deployed
+  res.send('3'); // change this string to ensure a new version deployed
 });
 
 // endpoint for doing an application health check
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw 'error...  ';
+  // // eslint-disable-next-line no-constant-condition
+  // if (true) throw 'error...  ';
   res.send('ok');
 });
 
