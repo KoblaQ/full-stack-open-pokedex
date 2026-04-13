c0e401fa1f65c1e01395cbb951870e662a139363
Edem <edem.quashigah@student.hamk.fi>
2026-04-13 20:21:26 +0300

    changed healthcheck and broke health check

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.get('/version', (req, res) => {
 });
 
 // endpoint for doing an application health check
-app.get('/health', (req, res) => {
+// app.get('/health', (req, res) => {
+app.get('/healthhh', (req, res) => {
   // // eslint-disable-next-line no-constant-condition
   // if (true) throw 'error...  ';
   res.send('ok');
